e7142ad9cfc8afbe8b4d0fc014236d6599e55374
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-07-31 17:06:27 +0330

    some changes added to project

--- zoom effect/script.js
+++ zoom effect/script.js
@@ -1,12 +1,17 @@
 window.onload = function(){
     const container = document.getElementById('container');
     const image = document.querySelector('img');
-    container.addEventListener('mousemove',function(Event){
-        const x = Event.clientX - Event.target.offsetLeft;//target = dive
-        const y = Event.clientY - Event.target.offsetTop;
-        //console.log(x,y);
-        image.style.transformOrigin = '${x}px ${y}px';//move when zoom on this image
+    container.addEventListener('mousemove',function(event){
+        const x = event.clientX - event.target.offsetLeft;//target = dive
+        const y = event.clientY - event.target.offsetTop;
+        console.log(x,y);
+        image.style.transformOrigin = `${x}px ${y}px`;//move when zoom on this image
         //image.style.transformOrigin = x + 'px'
-        image.style.transform  = 'scale(2)';//zoom
+        image.style.transform  = 'scale(2)';
+        
+    })
+    container.addEventListener('mouseleave',function(){
+        image.style.transformOrigin = 'center center';//move to default point
+        image.style.transform  = 'scale(1)';//zoom
     })
 }   
