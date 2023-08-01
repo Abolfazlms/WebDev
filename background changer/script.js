e5b1150c8ad016e17d5a9abb29186ede149cfd75
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-01 07:53:16 +0330

    js function to change color is written

--- background changer/script.js
+++ background changer/script.js
@@ -1,3 +1,9 @@
 window.onload = function(){
     const button = document.getElementById('button');
+    button.addEventListener('onclick',function(){
+        document.body.style.background = randomBackGround();
+    });
+}
+function randomBackGround(){
+    return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
 }
