c3098bc0fefb4af39db85f25bee9545cf0c59922
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-01 08:04:53 +0330

    autochange color feature added and comment in project

--- background changer/script.js
+++ background changer/script.js
@@ -1,15 +1,15 @@
-// window.onload = function(){
-//     const button = document.getElementById('button');
-//     button.addEventListener('click',function(){
-//         document.body.style.background = randomBackGround();
-//     });
-// }
-// function randomBackGround(){    
-//     //return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
-//     return`hsl(${Math.floor(Math.random()*360)},100%,50%)`;
-// }
+window.onload = function(){
+    const button = document.getElementById('button');
+    button.addEventListener('click',function(){
+        document.body.style.background = randomBackGround();
+    });
+}
 function randomBackGround(){    
     //return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
-    document.body.style.background = `hsl(${Math.floor(Math.random()*360)},100%,50%)`;
+    return`hsl(${Math.floor(Math.random()*360)},100%,50%)`;
 }
-setInterval(randomBackGround,500);
+// function randomBackGround(){    
+//     //return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+//     document.body.style.background = `hsl(${Math.floor(Math.random()*360)},100%,50%)`;
+// }
+// setInterval(randomBackGround,500);
