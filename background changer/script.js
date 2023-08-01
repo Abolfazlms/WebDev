ad452870c789c22e0dc04188e35f3cf769fee2f7
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-01 07:57:35 +0330

    onclick in js file changed to click

--- background changer/script.js
+++ background changer/script.js
@@ -1,9 +1,11 @@
+
 window.onload = function(){
     const button = document.getElementById('button');
-    button.addEventListener('onclick',function(){
+    button.addEventListener('click',function(){
         document.body.style.background = randomBackGround();
     });
 }
 function randomBackGround(){
+    alert('Hello World!');
     return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
 }
