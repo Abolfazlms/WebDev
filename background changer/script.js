8e984b274546aff929dd89d138f09dd03485be30
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-01 08:04:34 +0330

    autochange color feature added and comment in project

--- background changer/script.js
+++ background changer/script.js
@@ -1,10 +1,15 @@
-window.onload = function(){
-    const button = document.getElementById('button');
-    button.addEventListener('click',function(){
-        document.body.style.background = randomBackGround();
-    });
-}
+// window.onload = function(){
+//     const button = document.getElementById('button');
+//     button.addEventListener('click',function(){
+//         document.body.style.background = randomBackGround();
+//     });
+// }
+// function randomBackGround(){    
+//     //return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+//     return`hsl(${Math.floor(Math.random()*360)},100%,50%)`;
+// }
 function randomBackGround(){    
     //return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
-    return`hsl(${Math.floor(Math.random()*360)},100%,50%)`;
+    document.body.style.background = `hsl(${Math.floor(Math.random()*360)},100%,50%)`;
 }
+setInterval(randomBackGround,500);
