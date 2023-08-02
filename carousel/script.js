4596b0d29c7476762dff0b69d0c1dd8c1417f2a8
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-02 10:45:36 +0330

    animation and javascript code  added to this project

--- carousel/script.js
+++ carousel/script.js
@@ -1,3 +1,13 @@
 window.onload = function(){
-    
+    const images = document.getElementById('images');
+    const image = document.querySelectorAll('#images img')
+    let index = 0;
+    setInterval(run, 1500);
+    function run(){
+        index++;
+        if(index >= image.length-1){
+            index = 0;
+        }
+        images.style.transform = `translatex(${-index*500}px)`;
+    }
 }
