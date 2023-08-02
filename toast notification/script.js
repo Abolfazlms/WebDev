376f8f96ef94e6a5d681540fd29e85095fd0b8c7
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-02 08:11:02 +0330

    notification creation funtion added to js file and styled in css file

---
+++ toast notification/script.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    const button = document.getElementById('button');
+    const container = document.getElementById('container')
+    button.addEventListener('click',function(){
+        createNotification();
+    });
+    function createNotification(){
+        const notification = document.createElement('div');
+        notification.classList.add('toast');
+        notification.innerText = 'Test notification';
+        container.appendChild(notification);
+    }
+}
