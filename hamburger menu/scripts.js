d6a49c2634be4e44cd0b034c76de8d9697385b33
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-02 09:14:51 +0330

    some changes added to project

--- hamburger menu/scripts.js
+++ hamburger menu/scripts.js
@@ -1,3 +1,9 @@
 window.onload = function(){
+    const button = document.getElementById('button');
+    const nav = document.getElementById('nav');
     
+    button.addEventListener('click',function(){
+        nav.classList.toggle('active');
+        button.classList.toggle('active');
+    })
 }
